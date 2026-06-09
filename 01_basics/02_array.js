8d1ab38f612f722ad456b5662ebfe9a33cf8d065
crazy-titan <abhinavspl2003@gmail.com>
2026-06-09 11:30:47 +0530

    minor fixes

--- 01_basics/02_array.js
+++ 01_basics/02_array.js
@@ -15,7 +15,7 @@ const all_heros = marvel_heros.concat(dc_heoros);
 // console.log(all_heros);
 
 
-// Spread operator [...]
+// Spread operator [...] Recommended
 
 const all_new_heros = [...marvel_heros, ...dc_heoros]
-console.log(all_new_heros);
+// console.log(all_new_heros);
