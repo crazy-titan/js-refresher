3841ee4b2f48356043b12efbcf67ce4f8e6f22d9
crazy-titan <abhinavspl2003@gmail.com>
2026-06-09 15:01:25 +0530

    add switch-case concepts

---
+++ 03_control_flow/02_switch_case.js
@@ -0,0 +1,22 @@
+// 9-JUNE-2026
+
+// Switch-Case is used when you have a lot of if else conditions. So, remove the if-else and just go with switch-case
+
+const key= 3;
+
+switch (key) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Yes! its March");
+        break;
+    default:
+        console.log("its a Default value if nothing matches")
+        break;
+}
+
+// break is used to avoid the waterFall model of the programming language!
