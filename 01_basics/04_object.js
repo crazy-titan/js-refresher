ffa0f3cbf9d0216f37c7d33e46b6a3bca5475963
crazy-titan <abhinavspl2003@gmail.com>
2026-06-09 11:30:52 +0530

    add assign,keys,values,spread for Object

---
+++ 01_basics/04_object.js
@@ -0,0 +1,43 @@
+// 3-JUNE-2026
+
+// SingleTon using 'new' keyword
+
+// const myObj = new Object(); 
+
+const myObj = {};
+
+
+// Above line 5 and 7 both will give empty object on console.
+
+// add some data inside myObj
+
+myObj.name = "Abhinav"
+myObj.email = "contact@meetabhinav.com"
+myObj.isLoggedIn = true
+
+// console.log(myObj)
+
+
+// add multiple objects
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+// const obj3 = {obj1, obj2}; // not the good appraoch
+
+// const obj3 = Object.assign(obj1,obj2); // not the good approach
+// const obj3 = Object.assign({}.obj1,obj2); //  good approach to return in new object
+
+// we will use spread operatin again just like we did in Array(BEST APPROACH)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+// method to use the key,values and entites of object
+
+console.log(Object.keys(obj3)); // will show all key in ARRAY 
+
+console.log(Object.values(obj3)); // will show all values in ARRAY 
+
+console.log(Object.entries(obj3)); // will give the ARRAY of ARRAY in key:value pain.
