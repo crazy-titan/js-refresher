b9ce460b8295bda3acde3fa85c0da4f380dcdb67
crazy-titan <abhinavspl2003@gmail.com>
2026-06-09 11:31:02 +0530

    add object,array as argument in function

--- 02_basics/01_functions.js
+++ 02_basics/01_functions.js
@@ -25,9 +25,44 @@ function myFunction(name = "Abhinav"){
 
 // we can also use if-else
 
-if(name === undefined){
-    console.log('Please enter your name!')
-    return // directly come out of global scope
-}
+// if(name === undefined){
+//     console.log('Please enter your name!')
+//     return // directly come out of global scope
+// }
 
 // no argument is treated as 'undefined'
+
+
+// adding price of cart is tricky - beacuse you don't know how many arguments are coming!
+// we use rest operator ... same as spread operator
+
+function totalPriceOfCart(...carts){
+    return carts; // Arrays
+}
+// console.log(totalPriceOfCart(200,100,300))
+
+
+// passing objects and array inside a function
+
+const myObject = {
+    "name": "Abhinav",
+    "email": "contact@meetabhinav.com",
+    isloggedIn: true
+}
+
+function myFunctionOne(anyObject){
+    return `My name is ${anyObject.name} and email is ${anyObject.email}`
+}
+// console.log(myFunctionOne(myObject));
+// you can also pass the object directly into the function argument as ( {} )
+
+
+// Same for the Arrays
+
+const myArray = [100,200,34];
+
+function returnSecondValue(anyArray){
+    return anyArray[1];
+}
+// console.log(returnSecondValue(myArray));
+// you can also pass the array directly into the function's argument as ( [] )
