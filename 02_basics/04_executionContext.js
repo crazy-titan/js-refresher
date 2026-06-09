6f103a3cb85a6b0a6cc1e040ac3926582821c3c3
crazy-titan <abhinavspl2003@gmail.com>
2026-06-09 11:24:13 +0530

    add execution and call stack concept

---
+++ 02_basics/04_executionContext.js
@@ -0,0 +1,23 @@
+// 9-JUNE-2026
+
+// When a code runs first creation is the Global execution context!
+// Then we have two phase - MEMORY phase and EXECUTION phase 
+
+// Memory Phase
+// assigning the variables with 'undefinde'
+// functions assigned as there 'definations'
+
+
+// Execution Phase
+// assigning the value from undefined to mentioned value
+// functions will get another Execution Context Environment (again Memory and Execution Phase of it specific,
+// it will return into Global Execution context "THIS" and Environment gets deleted!)
+
+
+// And finally both phases laid to the final execution and output.
+
+
+// CALL STACK
+// nothing just a way how execution happens for example function execute and exit the stack, if there are 
+// nested function than all functions need to be exit first which are inside the parent function and at last
+// parent function get the exit.
