3d0ab4c06280ed62d0c1cfd0a40f8cb8c3513279
crazy-titan <abhinavspl2003@gmail.com>
2026-06-09 11:31:09 +0530

    add hosting,expression and general representation of function

--- 02_basics/02_scope.js
+++ 02_basics/02_scope.js
@@ -15,3 +15,38 @@ console.log(num,num2) // error not defined as they are inside the if statement's
 // KIM(Keep in Mind)
 
 // Browser environment always gives a different scope, where as NODE environment gives different scope.
+
+
+// Nested scope and Closers
+
+if(true){
+    const userName = "Abhinav";
+    if(userName === "Abhinav"){
+        const title = " LOL";
+        console.log(userName+title)
+    }
+    console.log(title); // give error because its outside the scope of nested if
+}
+console.log(userName); // give error beacuse its also outside the scope of parent if
+
+
+// Intersting things about Functions
+
+
+// we can access this type of function declaration from the top also
+
+addOne(4) // this will output as 5.
+function addOne(num){
+    return num + 1;
+}
+// addOne(5); // will give no output after execution because we are not printing the returned value.
+
+
+// 'Expression' way to define a function 
+
+// addTwo(6); // we can't do this because of 'hosting' and we stored the function inside a variable
+// so we can't access it before defining it!!!!
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(6); // this will output as 8
