b33b2dcd6b36cf1c296bf6c2d2d446e7093951aa
crazy-titan <abhinavspl2003@gmail.com>
2026-06-12 12:30:20 +0530

    add DOM intro

---
+++ 05_DOM/01_Intro.js
@@ -0,0 +1,17 @@
+// 12-JUNE-2026
+
+// DOM stands for Document Object Model, its known as Skelton of any Website!
+
+// Run on inspect 
+console.log(window); // this wiil output a whole data whatever you can think of inside a browser
+
+// but we mostly use document
+console.log(window.document); 
+//OR
+console.log(document);
+//OR
+console.dir(document); 
+
+// Run all three in browser and see the difference!
+
+// Keep in mind that the HTML Collection , Node List and Array are not same, they looks similar but not exactly same!
